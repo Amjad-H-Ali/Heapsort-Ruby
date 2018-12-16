9443c693445f97e35a7ff1f2802389eddccf77bb
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-12-15 20:51:39 -0600

    Defined max_heap function; defined largest variable; defined left and right node variables

--- heapsort.rb
+++ heapsort.rb
@@ -1,1 +1,18 @@
 p "HeapSort!"
+
+# Creates Max Heap
+# Meaning Parent Node is Greater Than Children Nodes
+# Params: an array, i: current index, n: size of array
+
+def max_heap array, i, n
+	# Assume i is the index that belongs to largest number
+	largest = i
+
+	# Define Left and Right Children
+
+	left = (i * 2) + 1
+
+	right = (i * 2) + 2
+
+	 
+end
